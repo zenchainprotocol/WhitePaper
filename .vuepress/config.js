a1cf9c419a5d94a96c12c4f74d1ce3df4bbf4a29
ZenChainDev <Zen@zenchain.co>
2021-02-24 23:08:15 +0700

    Public whitepaper

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,3 +1,20 @@
 module.exports = {
-
-  }
+    home: true,
+    title: 'Democracia. Manual de uso web',
+    base: '/',
+    themeConfig: {
+        nav: [
+            {text: 'Democracia', link: 'https://democracia.com.es'}
+        ],
+        sidebar: [
+            {
+                title: 'Contenido',
+                collapsable: false,
+                children: [
+                '/',
+                'front-page',
+                ]
+            },
+        ]
+    }
+}
